d3d9a7d24c539147b6a00b6c0413068613fbc500
Sakti Dwi Cahyono <me@saktidwicahyono.name>
2026-07-06 18:49:29 +0700

    ci: updates
    Signed-off-by: Sakti Dwi Cahyono <me@saktidwicahyono.name>

--- src/sakti/generate.py
+++ src/sakti/generate.py
@@ -13,7 +13,12 @@ TEMPLATE_FILE = Path(".") / "README.md.j2"
 OUTPUT_FILE = Path(".") / "README.md"
 
 def get_posts() -> list[dict]:
-    response = requests.get("https://saktidwicahyono.name/api/blogs/")
+    response = requests.get(
+        "https://saktidwicahyono.name/api/blogs/",
+        headers={
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0 Safari/537.36"
+        },
+    )
     response.raise_for_status()
     return response.json()["results"]
 
